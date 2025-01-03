6c8bb280a543f5b9e913fe9172bf405d8c1c6f62
Reuben Thomas <rrt@sc3d.org>
2025-01-03 18:37:20 +0000

    Remove unused import from future

--- rpl/__init__.py
+++ rpl/__init__.py
@@ -11,8 +11,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <https://www.gnu.org/licenses/>.
 
-from __future__ import annotations
-
 import argparse
 import importlib.metadata
 import io
